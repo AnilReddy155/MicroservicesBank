47c2b59f8ae4dbf624b508d0a4e69cb794be4fad
Anil <yarramreddy.anil@capgemini.com>
2024-06-11 17:04:46 +0530

    added API gateway

--- BankApp_AccountService_W2_Phase6/src/main/java/com/cg/bms/controller/AccountController.java
+++ BankApp_AccountService_W2_Phase6/src/main/java/com/cg/bms/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.cg.bms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,15 +28,23 @@ public class AccountController {
 
 	@Autowired
 	private AccountMgmService accountMgmService;
-	
+
 	@Autowired
 	private AccountRepository aaccountRepository;;
+	
+	@Autowired
+	private Environment environment;
 
 	// create endpoint to get all accounts
-		@GetMapping("abc")
-		public Account getAccount1() {
-			return aaccountRepository.getAccount(1000000000L);
-		}
+	@GetMapping("abc")
+	public Account getAccount1() {
+		Account account = aaccountRepository.getAccount(1000000000L);
+		
+		account.setAccountType(environment.getProperty("local.server.port"));
+		
+		return account;
+	}
+
 	// create endpoint to get all accounts
 	@GetMapping("/")
 	public List<AccountDto> getAccounts() throws AccountException {
@@ -47,50 +56,57 @@ public class AccountController {
 	public AccountDto getAccount(@PathVariable Long accountNumber) throws AccountException {
 		return new AccountDto(accountMgmService.getAccount(accountNumber));
 	}
-	
+
 	@GetMapping("/customer/{custId}")
 	public AccountDto getAccountByCustId(@PathVariable Long custId) throws AccountException {
 		return new AccountDto(accountMgmService.getAccountByCustId(custId));
 	}
-	
+
 	// create endpoint to get account balance
 	@GetMapping("/{accountNumber}/balance")
 	public Double getBalance(@PathVariable Long accountNumber) throws AccountException {
 		return accountMgmService.getBalance(accountNumber);
 	}
+
 	// create endpoint to deposit amount
 	@PostMapping("/{accountNumber}/deposit")
-	public AccountDto deposit(@PathVariable Long accountNumber,@RequestBody DepositRequest depositRequest) throws AccountException {
+	public AccountDto deposit(@PathVariable Long accountNumber, @RequestBody DepositRequest depositRequest)
+			throws AccountException {
 		return accountMgmService.deposit(accountNumber, depositRequest.getAmount());
 	}
-	
+
 	// create endpoint to withdraw amount
 	@PostMapping("/{accountNumber}/withdraw")
-	public AccountDto withdraw(@PathVariable Long accountNumber,@RequestBody WithdrawRequest withdrawRequest) throws AccountException {
+	public AccountDto withdraw(@PathVariable Long accountNumber, @RequestBody WithdrawRequest withdrawRequest)
+			throws AccountException {
 		return accountMgmService.withdraw(accountNumber, withdrawRequest.getAmount());
 	}
-	
+
 	// create endpoint to transfer amount
 	@PostMapping("/{fromAccountNumber}/transfer")
-	public AccountDto transfer(@PathVariable Long fromAccountNumber,@RequestBody TransferRequest transferRequest)
+	public AccountDto transfer(@PathVariable Long fromAccountNumber, @RequestBody TransferRequest transferRequest)
 			throws AccountException {
-		return accountMgmService.transfer(fromAccountNumber, transferRequest.getTargetAccount(), transferRequest.getAmount());
+		return accountMgmService.transfer(fromAccountNumber, transferRequest.getTargetAccount(),
+				transferRequest.getAmount());
 	}
 
 	// create endpoint to add account
 	@PostMapping("/")
 	public AccountDto addAccount(@RequestBody AccountDto accountDto) throws AccountException {
-		return accountMgmService.addAccount(accountDto.getAccountType(), accountDto.getBalance(), accountDto.getCustomerId());
+		return accountMgmService.addAccount(accountDto.getAccountType(), accountDto.getBalance(),
+				accountDto.getCustomerId());
 	}
+
 	// create endpoint to update account
 	@PutMapping("/")
 	public AccountDto updateAccount(@RequestBody AccountDto accountDto) throws AccountException {
 		return accountMgmService.updateAccount(accountDto);
 	}
+
 	// create endpoint to delete account
 	@DeleteMapping("/{accountNumber}")
 	public void deleteAccount(@PathVariable Long accountNumber) throws AccountException {
 		accountMgmService.deleteAccount(accountNumber);
 	}
-	
+
 }
